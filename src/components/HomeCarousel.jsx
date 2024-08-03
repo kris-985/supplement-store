73d633f0093c894bfbefcbc2dc0a2abbeb8795af
kris-985 <kris.bakalov98@gmail.com>
2024-08-03 03:17:59 +0300

    Fixed Carousel,Added stars and add,delete comments

--- src/components/HomeCarousel.jsx
+++ src/components/HomeCarousel.jsx
@@ -1,42 +1,42 @@
+import React from "react";
 import styled from "styled-components";
+import { Carousel } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export const HomeCarousel = () => {
-
   const images = [
-    'https://www.ironbrothers-shop.com/cdn/shop/files/WheyProteinIsolate_ChocoholicChampion.png?v=1699889015&width=1080',
-    'https://www.ironbrothers-shop.com/cdn/shop/files/WinterEdition_WheyProteinIsolate_Zimtstern.png?v=1699876022&width=1080',
-    'https://dukaan.b-cdn.net/700x700/webp/upload_file_service/asg/77553bd4-c99b-4828-b22f-20e6898a318d/Dexter-Jackson-Isolate-04.jpg',
-    'https://xplosiv.nz/media/catalog/product/cache/aadd22f13385e97bd2c31bcb82066b4a/x/p/xplosivisolate2lb_vanillarender.png'
+    "https://www.ironbrothers-shop.com/cdn/shop/files/WheyProteinIsolate_ChocoholicChampion.png?v=1699889015&width=1080",
+    "https://www.ironbrothers-shop.com/cdn/shop/files/WinterEdition_WheyProteinIsolate_Zimtstern.png?v=1699876022&width=1080",
+    "https://dukaan.b-cdn.net/700x700/webp/upload_file_service/asg/77553bd4-c99b-4828-b22f-20e6898a318d/Dexter-Jackson-Isolate-04.jpg",
+    "https://xplosiv.nz/media/catalog/product/cache/aadd22f13385e97bd2c31bcb82066b4a/x/p/xplosivisolate2lb_vanillarender.png",
   ];
 
   return (
-    <div id="carouselExampleIndicators" className="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="3000">
-      <div className="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-      </div>
-      <CarouselInner>
-        {images.map((image, index) => (
-          <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''} d-flex justify-content-center align-items-center`} style={{ height: '100%' }}>
-            <img src={image} alt={`Slide ${index + 1}`} className="img-fluid" style={{ maxHeight: '100%', maxWidth: '100%', objectFit: 'contain' }} />
-          </div>
-        ))}
-      </CarouselInner>
-      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
-    </div>
+    <Carousel interval={2000} fade>
+      {images.map((image, index) => (
+        <Carousel.Item key={index}>
+          <CarouselImageWrapper>
+            <img
+              src={image}
+              alt={`Slide ${index + 1}`}
+              className="d-block w-100"
+            />
+          </CarouselImageWrapper>
+        </Carousel.Item>
+      ))}
+    </Carousel>
   );
 };
 
-const CarouselInner = styled.div`
+const CarouselImageWrapper = styled.div`
   height: 500px;
-`;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
+  img {
+    max-height: 100%;
+    max-width: 100%;
+    object-fit: contain;
+  }
+`;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,21 +1,126 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // import { useCart } from './CartContext'; // Импортирай useCart хук
-import { products } from '../utils';
+import { products } from "../utils";
+
+const generateRandomStars = () => Math.floor(Math.random() * 6) + 1;
+
+const ProductCard = ({
+  product,
+  handleAddToCart,
+  comments,
+  addComment,
+  deleteComment,
+}) => {
+  const [comment, setComment] = useState("");
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+  };
+
+  const handleCommentSubmit = (event) => {
+    event.preventDefault();
+    addComment(product.name, comment);
+    setComment("");
+  };
+
+  return (
+    <div className="col-md-4 mb-4">
+      <div className="card h-100 shadow red-shadow border-3 border border-danger">
+        <img
+          src={product.picture || "https://via.placeholder.com/150"}
+          className="card-img-top"
+          alt={product.name}
+        />
+        <div className="card-body d-flex flex-column">
+          <div className="d-flex justify-content-between">
+            <h5 className="card-title">{product.name}</h5>
+            <span>{"⭐".repeat(product.stars)}</span>
+          </div>
+          <p className="card-text flex-grow-1">{product.description}</p>
+          <h6 className="card-subtitle mb-2 text-muted">
+            ${product.price.toFixed(2)}
+          </h6>
+          <button
+            className="btn btn-danger mt-auto"
+            onClick={() => handleAddToCart(product)}
+          >
+            Add to Cart
+          </button>
+          <div className="mt-3">
+            <h6>Comments:</h6>
+            <ul className="list-group mb-3">
+              {comments.map((comment, index) => (
+                <li
+                  key={index}
+                  className="list-group-item d-flex justify-content-between align-items-center"
+                >
+                  {comment}
+                  <button
+                    className="btn btn-sm btn-danger"
+                    onClick={() => deleteComment(product.name, index)}
+                  >
+                    Delete
+                  </button>
+                </li>
+              ))}
+            </ul>
+            <form onSubmit={handleCommentSubmit}>
+              <input
+                type="text"
+                className="form-control mb-2"
+                placeholder="Leave a comment"
+                value={comment}
+                onChange={handleCommentChange}
+              />
+              <button type="submit" className="btn btn-danger btn-sm">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export const Products = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [productComments, setProductComments] = useState({});
   // const { dispatch } = useCart(); // Достъп до dispatch функцията
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredProducts = products.filter((product) =>
-    product.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const filteredProducts = products
+    .map((product) => ({
+      ...product,
+      stars: generateRandomStars(),
+    }))
+    .filter((product) =>
+      product.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
   const handleAddToCart = (product) => {
-    dispatch({ type: 'ADD_ITEM', payload: product });
+    dispatch({ type: "ADD_ITEM", payload: product });
+  };
+
+  const addComment = (productName, comment) => {
+    setProductComments((prevComments) => ({
+      ...prevComments,
+      [productName]: [...(prevComments[productName] || []), comment],
+    }));
+  };
+
+  const deleteComment = (productName, commentIndex) => {
+    setProductComments((prevComments) => {
+      const updatedComments = [...prevComments[productName]];
+      updatedComments.splice(commentIndex, 1);
+      return {
+        ...prevComments,
+        [productName]: updatedComments,
+      };
+    });
   };
 
   return (
@@ -33,23 +138,14 @@ export const Products = () => {
       </div>
       <div className="row">
         {filteredProducts.map((product, index) => (
-          <div key={index} className="col-md-4 mb-4">
-            <div className="card h-100 shadow red-shadow border-3 border border-danger">
-              <img
-                src={product.picture || 'https://via.placeholder.com/150'}
-                className="card-img-top"
-                alt={product.name}
-              />
-              <div className="card-body d-flex flex-column">
-                <h5 className="card-title">{product.name}</h5>
-                <p className="card-text flex-grow-1">{product.description}</p>
-                <h6 className="card-subtitle mb-2 text-muted">${product.price.toFixed(2)}</h6>
-                <button className="btn btn-danger mt-auto" onClick={() => handleAddToCart(product)}>
-                  Add to Cart
-                </button>
-              </div>
-            </div>
-          </div>
+          <ProductCard
+            key={index}
+            product={product}
+            handleAddToCart={handleAddToCart}
+            comments={productComments[product.name] || []}
+            addComment={addComment}
+            deleteComment={deleteComment}
+          />
         ))}
       </div>
     </div>
